326058d4dfa3aa219d66de1448cd44c6d883c8c8
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-06-08 03:01:12 +0000

    commit68

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ HEPTA_EPS eps(PA_0,PA_4);
 Serial pc(USBTX,USBRX,9600);
 int main()
 {
-    float btvol;
+    float bt;
     for(int i=0;i<10;i++) {
-        eps.vol(&btvol);
-        pc.printf("Voltage = %f\r\n",btvol);
-        wait_ms(1000);
+        eps.vol(&bt);
+        pc.printf("Vol = %f\r\n",bt);
+        wait(1);
     }
 }
