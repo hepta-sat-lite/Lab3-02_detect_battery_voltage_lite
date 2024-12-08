ed4d8aef0386777fdb21c6c748d430d3042facfb
Charlie.com <csru22005@g.nihon-u.ac.jp>
2024-12-08 05:09:59 +0000

    fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include "mbed.h"
 #include "LITE_EPS.h"
-LITE_EPS eps(PA_0,PA_4);
+
 RawSerial pc(USBTX,USBRX,9600);
+LITE_EPS eps(PA_0,PA_4);
+
 int main()
 {
     float bt;
-    for(int i=0;i<10;i++) {
+    for(int i = 0; i < 10; i++) {
         eps.vol(&bt);
         pc.printf("Vol = %f\r\n",bt);
-        wait(1);
+        wait(1.0);
     }
 }
